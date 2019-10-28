27a3e36233274c6d95b9486495d901d4b56dabda
mtei <2170248+mtei@users.noreply.github.com>
2019-10-28 15:06:01 +0900

    Added option to use split_common for helix keyboard.
    how to build:
      ### build helix/pico (HelixPico) with split_common codes
      $ make helix/pico/sc:KEY_MAP
      $ make helix/pico/sc/back:KEY_MAP
      $ make helix/pico/sc/under:KEY_MAP
    
      ### build helix/rev2 (Helix) with split_common codes
      $ make helix/rev2/sc:KEY_MAP
      $ make helix/rev2/sc/back:KEY_MAP
      $ make helix/rev2/sc/under:KEY_MAP
      $ make helix/rev2/sc/oled:KEY_MAP
      $ make helix/rev2/sc/oledback:KEY_MAP
      $ make helix/rev2/sc/oledunder:KEY_MAP
    
      ### build helix/rev1 (Helix alpha)
      $ make helix/rev1:KEY_MAP
    
      ### build helix/pico (HelixPico) with helix old codes
      $ make helix/pico:KEY_MAP
      $ make helix/pico/back:KEY_MAP
    
      ### build helix/rev2 (Helix or Helix beta) with helix old codes
      $ make helix:KEY_MAP
      $ make helix/rev2/back:KEY_MAP
      $ make helix/rev2/under:KEY_MAP
      $ make helix/rev2/oled:KEY_MAP
      $ make helix/rev2/oled/back:KEY_MAP
      $ make helix/rev2/oled/under:KEY_MAP

--- keyboards/helix/pico/local_features.mk
+++ keyboards/helix/pico/local_features.mk
@@ -53,6 +53,16 @@ endef
     SHOW_HELIX_OPTIONS = yes
   endif
 
+ifneq ($(strip $(SPLIT_KEYBOARD)), yes)
+  SRC += local_drivers/serial.c
+  KEYBOARD_PATHS += $(HELIX_TOP_DIR)/local_drivers
+
+  CUSTOM_MATRIX = yes
+
+  SRC += pico/matrix.c
+  SRC += pico/split_util.c
+endif
+
 ########
 # convert Helix-specific options (that represent combinations of standard options)
 #   into QMK standard options.
--- keyboards/helix/rev2/keymaps/xulkal/rules.mk
+++ keyboards/helix/rev2/keymaps/xulkal/rules.mk
@@ -10,4 +10,12 @@ OPT_DEFS += -DOLED_FONT_H=\"common/glcdfont.c\"
 # Xulkal specific oled define
 OPT_DEFS += -DOLED_90ROTATION
 
-DISABLE_LTO = yes
+ifneq ($(strip $(SPLIT_KEYBOARD)), yes)
+  CUSTOM_MATRIX = yes  # use Helix old code
+
+  LIB_SRC += local_drivers/serial.c
+  KEYBOARD_PATHS += $(HELIX_TOP_DIR)/local_drivers
+  SRC += rev2/matrix.c
+  SRC += rev2/split_util.c
+  SRC += rev2/split_scomm.c
+endif
