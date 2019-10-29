02e470febed2c6dc9b258db94bb4dab32ee86fed
mtei <2170248+mtei@users.noreply.github.com>
2019-10-29 23:10:36 +0900

    Changed helix/pico to use is_keyboard_master() instead of is_master.

--- keyboards/helix/pico/split_util.h
+++ keyboards/helix/pico/split_util.h
@@ -7,13 +7,12 @@
 #define SLAVE_I2C_ADDRESS           0x32
 
 extern volatile bool isLeftHand;
+extern volatile bool is_master;
 
 // slave version of matix scan, defined in matrix.c
 void matrix_slave_scan(void);
 
 void split_keyboard_setup(void);
 bool has_usb(void);
-
-void matrix_master_OLED_init (void);
 
 #endif
