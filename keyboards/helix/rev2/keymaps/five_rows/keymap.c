c90920c51d0b0c0f076afe0477af7133b3d7a04a
mtei <2170248+mtei@users.noreply.github.com>
2019-10-30 22:40:25 +0900

    Change helix/rev2:five_rows OLE display

--- keyboards/helix/rev2/keymaps/five_rows/keymap.c
+++ keyboards/helix/rev2/keymaps/five_rows/keymap.c
@@ -503,35 +503,6 @@ void matrix_update(struct CharacterMatrix *dest,
 }
 #endif
 
-#ifdef SSD1306OLED
-static void render_logo(struct CharacterMatrix *matrix) {
-#else
-static void render_logo(void) {
-#endif
-
-  static char logo[]={
-    0x80,0x81,0x82,0x83,0x84,0x85,0x86,0x87,0x88,0x89,0x8a,0x8b,0x8c,0x8d,0x8e,0x8f,0x90,0x91,0x92,0x93,0x94,
-    0xa0,0xa1,0xa2,0xa3,0xa4,0xa5,0xa6,0xa7,0xa8,0xa9,0xaa,0xab,0xac,0xad,0xae,0xaf,0xb0,0xb1,0xb2,0xb3,0xb4,
-    0xc0,0xc1,0xc2,0xc3,0xc4,0xc5,0xc6,0xc7,0xc8,0xc9,0xca,0xcb,0xcc,0xcd,0xce,0xcf,0xd0,0xd1,0xd2,0xd3,0xd4,
-    0};
-  oled_write(logo, false);
-#if defined(RGBLIGHT_ENABLE) && defined(RGBLIGHT_ANIMATIONS)
-  char buf[30];
-  if(rgblight_config.enable) {
-      snprintf(buf, sizeof(buf), " LED %2d: %d,%d,%d ",
-               rgblight_config.mode,
-               rgblight_config.hue/RGBLIGHT_HUE_STEP,
-               rgblight_config.sat/RGBLIGHT_SAT_STEP,
-               rgblight_config.val/RGBLIGHT_VAL_STEP);
-      oled_write(buf, false);
-#ifndef SSD1306OLED
-  } else {
-      oled_write_P( PSTR("\n"), false);
-#endif
-  }
-#endif
-}
-
 static const char Qwerty_name[]  PROGMEM = " Qwerty";
 static const char Colemak_name[] PROGMEM = " Colemak";
 static const char Dvorak_name[]  PROGMEM = " Dvorak";
@@ -560,6 +531,63 @@ static const char *layer_names[] = {
 };
 
 #ifdef SSD1306OLED
+void render_layer_status(struct CharacterMatrix *matrix) {
+#else
+void render_layer_status(void) {
+#endif
+  int name_num;
+  uint32_t lstate;
+  for( lstate = layer_state, name_num = 0;
+       lstate && name_num < sizeof(layer_names)/sizeof(char *);
+       lstate >>=1, name_num++ ) {
+      if( (lstate & 1) != 0 ) {
+          if( layer_names[name_num] ) {
+              oled_write_P(layer_names[name_num], false);
+          }
+      }
+  }
+}
+
+#ifdef SSD1306OLED
+static void render_logo(struct CharacterMatrix *matrix) {
+#else
+static void render_logo(void) {
+#endif
+
+  static char logo[]={
+    0x80,0x81,0x82,0x83,0x84,0x85,0x86,0x87,0x88,0x89,0x8a,0x8b,0x8c,0x8d,0x8e,0x8f,0x90,0x91,0x92,0x93,0x94,
+    0xa0,0xa1,0xa2,0xa3,0xa4,0xa5,0xa6,0xa7,0xa8,0xa9,0xaa,0xab,0xac,0xad,0xae,0xaf,0xb0,0xb1,0xb2,0xb3,0xb4,
+    0xc0,0xc1,0xc2,0xc3,0xc4,0xc5,0xc6,0xc7,0xc8,0xc9,0xca,0xcb,0xcc,0xcd,0xce,0xcf,0xd0,0xd1,0xd2,0xd3,0xd4,
+    0};
+  oled_write(logo, false);
+#if defined(RGBLIGHT_ENABLE) && defined(RGBLIGHT_ANIMATIONS)
+  char buf[30];
+  if(rgblight_config.enable) {
+      snprintf(buf, sizeof(buf), " LED %2d: %d,%d,%d ",
+               rgblight_config.mode,
+               rgblight_config.hue/RGBLIGHT_HUE_STEP,
+               rgblight_config.sat/RGBLIGHT_SAT_STEP,
+               rgblight_config.val/RGBLIGHT_VAL_STEP);
+      oled_write(buf, false);
+  } else {
+#  ifdef SSD1306OLED
+      render_layer_status(matrix);
+#  else
+      render_layer_status();
+      oled_write_P(PSTR("\n"), false);
+#  endif
+  }
+#else // ! defined(RGBLIGHT_ENABLE) && defined(RGBLIGHT_ANIMATIONS)
+#  ifdef SSD1306OLED
+  render_layer_status(matrix);
+#  else
+  render_layer_status();
+  oled_write_P(PSTR("\n"), false);
+#  endif
+#endif
+}
+
+#ifdef SSD1306OLED
 void render_status(struct CharacterMatrix *matrix) {
 #else
 void render_status(void) {
@@ -577,21 +605,13 @@ void render_status(void) {
     oled_write(logo[1][1], false);
   }
 
-  // Define layers here, Have not worked out how to have text displayed for each layer. Copy down the number you see and add a case for it below
-  int name_num;
-  uint32_t lstate;
   oled_write_P(layer_names[current_default_layer], false);
   oled_write_P(PSTR("\n"), false);
-  for( lstate = layer_state, name_num = 0;
-       lstate && name_num < sizeof(layer_names)/sizeof(char *);
-       lstate >>=1, name_num++ ) {
-      if( (lstate & 1) != 0 ) {
-          if( layer_names[name_num] ) {
-              oled_write_P(layer_names[name_num], false);
-          }
-      }
-  }
-
+#ifdef SSD1306OLED
+  render_layer_status(matrix);
+#else
+  render_layer_status();
+#endif
   // Host Keyboard LED Status
   char led[40];
     snprintf(led, sizeof(led), "\n%s  %s  %s",
