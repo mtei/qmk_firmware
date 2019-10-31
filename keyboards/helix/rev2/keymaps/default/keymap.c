f245295e2422655dd7f4f5af90fe763c3d188201
mtei <2170248+mtei@users.noreply.github.com>
2019-10-31 19:46:16 +0900

    Change helix/rev2:default OLE display code for save RAM (display contents no change)

--- keyboards/helix/rev2/keymaps/default/keymap.c
+++ keyboards/helix/rev2/keymaps/default/keymap.c
@@ -546,12 +546,12 @@ static void render_logo(struct CharacterMatrix *matrix) {
 static void render_logo(void) {
 #endif
 
-  static char logo[]={
+  static const char helix_logo[] PROGMEM ={
     0x80,0x81,0x82,0x83,0x84,0x85,0x86,0x87,0x88,0x89,0x8a,0x8b,0x8c,0x8d,0x8e,0x8f,0x90,0x91,0x92,0x93,0x94,
     0xa0,0xa1,0xa2,0xa3,0xa4,0xa5,0xa6,0xa7,0xa8,0xa9,0xaa,0xab,0xac,0xad,0xae,0xaf,0xb0,0xb1,0xb2,0xb3,0xb4,
     0xc0,0xc1,0xc2,0xc3,0xc4,0xc5,0xc6,0xc7,0xc8,0xc9,0xca,0xcb,0xcc,0xcd,0xce,0xcf,0xd0,0xd1,0xd2,0xd3,0xd4,
     0};
-  oled_write(logo, false);
+  oled_write_P(helix_logo, false);
 }
 
 
@@ -563,20 +563,19 @@ void render_status(void) {
 #endif
 
   // Render to mode icon
-  static char logo[][2][3]={{{0x95,0x96,0},{0xb5,0xb6,0}},{{0x97,0x98,0},{0xb7,0xb8,0}}};
+  static const char os_logo[][2][3] PROGMEM ={{{0x95,0x96,0},{0xb5,0xb6,0}},{{0x97,0x98,0},{0xb7,0xb8,0}}};
   if(keymap_config.swap_lalt_lgui==false){
-    oled_write(logo[0][0], false);
+    oled_write_P(os_logo[0][0], false);
     oled_write_P(PSTR("\n"), false);
-    oled_write(logo[0][1], false);
+    oled_write_P(os_logo[0][1], false);
   }else{
-    oled_write(logo[1][0], false);
+    oled_write_P(os_logo[1][0], false);
     oled_write_P(PSTR("\n"), false);
-    oled_write(logo[1][1], false);
+    oled_write_P(os_logo[1][1], false);
   }
 
   // Define layers here, Have not worked out how to have text displayed for each layer. Copy down the number you see and add a case for it below
-  char buf[40];
-  snprintf(buf,sizeof(buf), "Undef-%ld", layer_state);
+  char buf[10];
   oled_write_P(PSTR("\nLayer: "), false);
     switch (layer_state) {
         case L_BASE:
@@ -593,16 +592,19 @@ void render_status(void) {
            oled_write_P(PSTR("Adjust"), false);
            break;
         default:
+           oled_write_P(PSTR("Undef-"), false);
+           snprintf(buf,sizeof(buf), "%ld", layer_state);
            oled_write(buf, false);
     }
 
   // Host Keyboard LED Status
-  char led[40];
-    snprintf(led, sizeof(led), "\n%s  %s  %s",
-            (host_keyboard_leds() & (1<<USB_LED_NUM_LOCK)) ? "NUMLOCK" : "       ",
-            (host_keyboard_leds() & (1<<USB_LED_CAPS_LOCK)) ? "CAPS" : "    ",
-            (host_keyboard_leds() & (1<<USB_LED_SCROLL_LOCK)) ? "SCLK" : "    ");
-  oled_write(led, false);
+  oled_write_P(PSTR("\n"), false);
+  oled_write_P((host_keyboard_leds() & (1<<USB_LED_NUM_LOCK)) ?
+               PSTR("NUMLOCK") : PSTR("       "), false);
+  oled_write_P((host_keyboard_leds() & (1<<USB_LED_CAPS_LOCK)) ?
+               PSTR("CAPS") : PSTR("    "), false);
+  oled_write_P((host_keyboard_leds() & (1<<USB_LED_SCROLL_LOCK)) ?
+               PSTR("SCLK") : PSTR("    "), false);
 }
 
 
