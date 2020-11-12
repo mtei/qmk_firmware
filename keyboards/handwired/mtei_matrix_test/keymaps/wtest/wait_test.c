d3efa24791af236c787924469e19f2a0ddeae95e
mtei <2170248+mtei@users.noreply.github.com>
2020-11-13 03:20:45 +0900

    update mtei_matrix_test/keymaps/wtest/wait_test.c

--- keyboards/handwired/mtei_matrix_test/keymaps/wtest/wait_test.c
+++ keyboards/handwired/mtei_matrix_test/keymaps/wtest/wait_test.c
@@ -1,7 +1,10 @@
 #include QMK_KEYBOARD_H
 #include "wait.h"
 
-#define TEST_INTERVAL 1000
+#define WAIT_NUM_BASE 6
+#define WAIT_NUM_LOOP 4
+
+#define TEST_INTERVAL 500
 
 void wait_test_0(void)  { writePinHigh(WAIT_TSET_PIN); wait_cpuclock(0); writePinLow(WAIT_TSET_PIN); }
 void wait_test_1(void)  { writePinHigh(WAIT_TSET_PIN); wait_cpuclock(1); writePinLow(WAIT_TSET_PIN); }
@@ -23,6 +26,73 @@ void wait_test_16(void) { writePinHigh(WAIT_TSET_PIN); wait_cpuclock(16); writeP
 void wait_test_17(void) { writePinHigh(WAIT_TSET_PIN); wait_cpuclock(17); writePinLow(WAIT_TSET_PIN); }
 void wait_test_18(void) { writePinHigh(WAIT_TSET_PIN); wait_cpuclock(18); writePinLow(WAIT_TSET_PIN); }
 void wait_test_19(void) { writePinHigh(WAIT_TSET_PIN); wait_cpuclock(19); writePinLow(WAIT_TSET_PIN); }
+void wait_test_20(void) { writePinHigh(WAIT_TSET_PIN); wait_cpuclock(20); writePinLow(WAIT_TSET_PIN); }
+void wait_test_21(void) { writePinHigh(WAIT_TSET_PIN); wait_cpuclock(21); writePinLow(WAIT_TSET_PIN); }
+void wait_test_22(void) { writePinHigh(WAIT_TSET_PIN); wait_cpuclock(22); writePinLow(WAIT_TSET_PIN); }
+void wait_test_23(void) { writePinHigh(WAIT_TSET_PIN); wait_cpuclock(23); writePinLow(WAIT_TSET_PIN); }
+void wait_test_24(void) { writePinHigh(WAIT_TSET_PIN); wait_cpuclock(24); writePinLow(WAIT_TSET_PIN); }
+void wait_test_25(void) { writePinHigh(WAIT_TSET_PIN); wait_cpuclock(25); writePinLow(WAIT_TSET_PIN); }
+
+void wait_test_64(void) { writePinHigh(WAIT_TSET_PIN); wait_cpuclock(64); writePinLow(WAIT_TSET_PIN); }
+void wait_test_65(void) { writePinHigh(WAIT_TSET_PIN); wait_cpuclock(65); writePinLow(WAIT_TSET_PIN); }
+
+void wait_test_72(void) { writePinHigh(WAIT_TSET_PIN); wait_cpuclock(65); writePinLow(WAIT_TSET_PIN); }
+
+void wait_test_16_p(void) {
+    writePinHigh(WAIT_TSET_PIN);
+    wait_cpuclock(8);
+    wait_cpuclock(8);
+    writePinLow(WAIT_TSET_PIN);
+}
+
+void wait_test_24_p(void) {
+    writePinHigh(WAIT_TSET_PIN);
+    wait_cpuclock(8);
+    wait_cpuclock(8);
+    wait_cpuclock(8);
+    writePinLow(WAIT_TSET_PIN);
+}
+
+void wait_test_64_p(void) {
+    writePinHigh(WAIT_TSET_PIN);
+    wait_cpuclock(8);
+    wait_cpuclock(8);
+    wait_cpuclock(8);
+    wait_cpuclock(8);//4
+    wait_cpuclock(8);
+    wait_cpuclock(8);
+    wait_cpuclock(8);
+    wait_cpuclock(8);//8
+    writePinLow(WAIT_TSET_PIN);
+}
+
+void wait_test_65_p(void) {
+    writePinHigh(WAIT_TSET_PIN);
+    wait_cpuclock(8);
+    wait_cpuclock(8);
+    wait_cpuclock(8);
+    wait_cpuclock(8);//4
+    wait_cpuclock(8);
+    wait_cpuclock(8);
+    wait_cpuclock(8);
+    wait_cpuclock(8);//8
+    wait_cpuclock(1);
+    writePinLow(WAIT_TSET_PIN);
+}
+
+void wait_test_72_p(void) {
+    writePinHigh(WAIT_TSET_PIN);
+    wait_cpuclock(8);
+    wait_cpuclock(8);
+    wait_cpuclock(8);
+    wait_cpuclock(8);//4
+    wait_cpuclock(8);
+    wait_cpuclock(8);
+    wait_cpuclock(8);
+    wait_cpuclock(8);//8
+    wait_cpuclock(8);
+    writePinLow(WAIT_TSET_PIN);
+}
 
 void matrix_scan_post_user(void) {
     static int testnum = 0;
@@ -31,7 +101,7 @@ void matrix_scan_post_user(void) {
         return;
     }
     test_timeout = timer_read();
-    switch(testnum) {
+    switch(testnum+WAIT_NUM_BASE) {
     case 0: wait_test_0(); break;
     case 1: wait_test_1(); break;
     case 2: wait_test_2(); break;
@@ -41,7 +111,7 @@ void matrix_scan_post_user(void) {
     case 6: wait_test_6(); break;
     case 7: wait_test_7(); break;
     case 8: wait_test_8(); break;
-    case 9: wait_test_9(); break;
+    case 9: wait_test_9();   break;
     case 10: wait_test_10(); break;
     case 11: wait_test_11(); break;
     case 12: wait_test_12(); break;
@@ -52,8 +122,14 @@ void matrix_scan_post_user(void) {
     case 17: wait_test_17(); break;
     case 18: wait_test_18(); break;
     case 19: wait_test_19(); break;
+    case 20: wait_test_20(); break;
+    case 21: wait_test_21(); break;
+    case 22: wait_test_22(); break;
+    case 23: wait_test_23(); break;
+    case 24: wait_test_24(); break;
+    case 25: wait_test_25(); break;
     }
-    testnum = testnum % 20;
+    testnum = (testnum + 1) % WAIT_NUM_LOOP;
 }
 
 void keyboard_post_init_user() {
