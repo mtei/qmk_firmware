e127c29ebfa1115380465fa626b8b111c368c96b
mtei <2170248+mtei@users.noreply.github.com>
2020-11-13 04:54:09 +0900

    update mtei_matrix_test/keymaps/wtest/aligned_4clock_delay.c
    add aligned_16clock_delay()

--- keyboards/handwired/mtei_matrix_test/keymaps/wtest/aligned_4clock_delay.c
+++ keyboards/handwired/mtei_matrix_test/keymaps/wtest/aligned_4clock_delay.c
@@ -1,9 +1,10 @@
+/* clang-format off */
 #if defined(__ARM_ARCH_7M__) || defined(__ARM_ARCH_7EM__)
   #ifndef __OPTIMIZE__
     #error "Compiler optimizations disabled; wait_cpuclock() won't work as designed"
   #endif
 
-__attribute__ ((aligned(4),noinline))
+__attribute__ ((aligned(16),noinline))
 void aligned_4clock_delay(unsigned int n)
 {
     do {
@@ -11,4 +12,27 @@ void aligned_4clock_delay(unsigned int n)
     } while ( --n > 0 );
     asm volatile (""::: "memory");
 }
+
+__attribute__ ((aligned(16),noinline))
+void aligned_8clock_delay(unsigned int n)
+{
+    do {
+        asm volatile ("nop\n nop\n nop\n nop\n"::: "memory");
+        asm volatile ("nop"::: "memory");
+    } while ( --n > 0 );
+    asm volatile (""::: "memory");
+}
+
+__attribute__ ((aligned(16),noinline))
+void aligned_16clock_delay(unsigned int n)
+{
+    do {
+        asm volatile ("nop\n nop\n nop\n nop\n"::: "memory");
+        asm volatile ("nop\n nop\n nop\n nop\n"::: "memory");
+        asm volatile ("nop\n nop\n nop\n nop\n"::: "memory");
+        asm volatile ("nop"::: "memory");
+    } while ( --n > 0 );
+    asm volatile (""::: "memory");
+}
+
 #endif
