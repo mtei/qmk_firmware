9bffdd77aae193ccb8efcbd505a230478413e1f3
mtei <2170248+mtei@users.noreply.github.com>
2020-11-18 13:00:18 +0900

    fix mtei_matrix_test/wait.h for Proton-C normal clock
    Proton-C の通常の設定で、all nop タイプと loop タイプで、1マイクロ秒まで結果が一致した。

--- keyboards/handwired/mtei_matrix_test/keymaps/wtest/rules.mk
+++ keyboards/handwired/mtei_matrix_test/keymaps/wtest/rules.mk
@@ -1,4 +1,6 @@
-SRC += wait_test.c aligned_4clock_delay.c
+CONSOLE_ENABLE = yes
+
+SRC += wait_test.c aligned_nop_loop.c
 
 ifneq ($(strip $(TEST)),)
   define KEYMAP_OPTION_PARSE
@@ -15,11 +17,8 @@ ifneq ($(strip $(TEST)),)
     ifeq ($(strip $1),long)
         TEST_TYPE = long
     endif
-    ifeq ($(strip $1),4clock)
-        TEST_TYPE = 4clock
-    endif
-    ifeq ($(strip $1),16clock)
-        TEST_TYPE = 16clock
+    ifeq ($(strip $1),loop)
+        TEST_TYPE = loop
     endif
     ifeq ($(strip $1),align)
         TEST_TYPE = align
@@ -40,11 +39,8 @@ endif
 ifeq ($(strip $(TEST_TYPE)),long)
   OPT_DEFS += -DLONG_TEST
 endif
-ifeq ($(strip $(TEST_TYPE)),4clock)
-  OPT_DEFS += -DTEST_4CLOCK
-endif
-ifeq ($(strip $(TEST_TYPE)),16clock)
-  OPT_DEFS += -DTEST_16CLOCK
+ifeq ($(strip $(TEST_TYPE)),loop)
+  OPT_DEFS += -DLOOP_TEST
 endif
 ifeq ($(strip $(TEST_TYPE)),align)
   OPT_DEFS += -DALIGN_TEST
--- keyboards/handwired/mtei_matrix_test/wait.h
+++ keyboards/handwired/mtei_matrix_test/wait.h
@@ -7,6 +7,8 @@
 extern "C" {
 #endif
 
+#define ALIGNED_NOP_LOOP_CLOCKS 9
+#define ALIGNED_NOP_LOOP_CALL_OVER_HEAD 8
 
 // AVR
 #if defined(__AVR__)
@@ -19,31 +21,56 @@ extern "C" {
     #error "Compiler optimizations disabled; wait_cpuclock() won't work as designed"
   #endif
 
-extern void aligned_4clock_delay(unsigned int n);
-extern void aligned_16clock_delay(unsigned int n);
-#define OVER_HEAD_CALL_4CLOCK_DELAY 4
+extern void aligned_nop_loop(unsigned int n);
+
+#ifdef ALIGNED_NOP_LOOP_CALL_OVER_HEAD
+#    if ALIGNED_NOP_LOOP_CALL_OVER_HEAD < 0 || (ALIGNED_NOP_LOOP_CALL_OVER_HEAD + ALIGNED_NOP_LOOP_CLOCKS) > 24
+#        error Invalid ALIGNED_NOP_LOOP_CALL_OVER_HEAD value
+#    endif
+#else
+#    define ALIGNED_NOP_LOOP_CALL_OVER_HEAD 4
+#endif
+
+
+#define WAIT_EXPANDING_NOP_24(n) do { switch (n) { \
+    case 24: asm volatile ("nop"::: "memory"); \
+    case 23: asm volatile ("nop"::: "memory"); \
+    case 22: asm volatile ("nop"::: "memory"); \
+    case 21: asm volatile ("nop"::: "memory"); \
+    case 20: asm volatile ("nop"::: "memory"); \
+    case 19: asm volatile ("nop"::: "memory"); \
+    case 18: asm volatile ("nop"::: "memory"); \
+    case 17: asm volatile ("nop"::: "memory"); \
+    case 16: asm volatile ("nop"::: "memory"); \
+    case 15: asm volatile ("nop"::: "memory"); \
+    case 14: asm volatile ("nop"::: "memory"); \
+    case 13: asm volatile ("nop"::: "memory"); \
+    case 12: asm volatile ("nop"::: "memory"); \
+    case 11: asm volatile ("nop"::: "memory"); \
+    case 10: asm volatile ("nop"::: "memory"); \
+    case  9: asm volatile ("nop"::: "memory"); \
+    case  8: asm volatile ("nop"::: "memory"); \
+    case  7: asm volatile ("nop"::: "memory"); \
+    case  6: asm volatile ("nop"::: "memory"); \
+    case  5: asm volatile ("nop"::: "memory"); \
+    case  4: asm volatile ("nop"::: "memory"); \
+    case  3: asm volatile ("nop"::: "memory"); \
+    case  2: asm volatile ("nop"::: "memory"); \
+    case  1: asm volatile ("nop"::: "memory"); \
+    case  0: break; \
+    } } while(0)
 
 __attribute__((always_inline))
-static inline void wait_cpuclock_4noploop(unsigned int n) {
+static inline void wait_cpuclock_noploop(unsigned int n) {
     /* The argument n must be a constant expression.
      * That way, compiler optimization will remove unnecessary code. */
     if (n < 1) { return; }
-    if (n > 8) {
-        unsigned int n4 = (n - OVER_HEAD_CALL_4CLOCK_DELAY)/4;
-        n = n - n4*4 - OVER_HEAD_CALL_4CLOCK_DELAY;
-        aligned_4clock_delay(n4);
+    if (n > ((ALIGNED_NOP_LOOP_CALL_OVER_HEAD)+(ALIGNED_NOP_LOOP_CLOCKS))) {
+        unsigned int loop = (n - (ALIGNED_NOP_LOOP_CALL_OVER_HEAD))/(ALIGNED_NOP_LOOP_CLOCKS);
+        n = n - loop*(ALIGNED_NOP_LOOP_CLOCKS) - (ALIGNED_NOP_LOOP_CALL_OVER_HEAD);
+        aligned_nop_loop(loop);
     }
-    switch (n) {
-    case 8: asm volatile ("nop"::: "memory");
-    case 7: asm volatile ("nop"::: "memory");
-    case 6: asm volatile ("nop"::: "memory");
-    case 5: asm volatile ("nop"::: "memory");
-    case 4: asm volatile ("nop"::: "memory");
-    case 3: asm volatile ("nop"::: "memory");
-    case 2: asm volatile ("nop"::: "memory");
-    case 1: asm volatile ("nop"::: "memory");
-    case 0: break;
-    }
+    WAIT_EXPANDING_NOP_24(n);
 }
 
     // Table 3.1. Cortex-M3 instruction set summary
