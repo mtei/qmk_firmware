5a6d845d21b5ad56dd1bb4257e030e03fff14f7e
mtei <2170248+mtei@users.noreply.github.com>
2020-11-21 18:29:03 +0900

    mtei_matrix_test/keymaps/wtest/aligned_nop_loop.c: use ASM

--- keyboards/handwired/mtei_matrix_test/keymaps/wtest/aligned_nop_loop.c
+++ keyboards/handwired/mtei_matrix_test/keymaps/wtest/aligned_nop_loop.c
@@ -1,9 +1,10 @@
 /* clang-format off */
 
 #if defined(__ARM_ARCH_7M__) || defined(__ARM_ARCH_7EM__)
-  #ifndef __OPTIMIZE__
-    #error "Compiler optimizations disabled; wait_cpuclock() won't work as designed"
-  #endif
+#    if 0 /* C or ASM switch */
+#        ifndef __OPTIMIZE__
+#            error "Compiler optimizations disabled; wait_cpuclock() won't work as designed"
+#        endif
 
 __attribute__ ((aligned(16),noinline))
 void aligned_nop_loop(unsigned int n)
@@ -16,4 +17,21 @@ void aligned_nop_loop(unsigned int n)
     asm volatile (""::: "memory");
 }
 
-#endif
+#    else  /* ASM */
+__attribute__ ((aligned(16),noinline))
+void aligned_nop_loop(unsigned int n)
+{
+    asm volatile (
+                  "0:                          \n\t"
+                  "      nop                   \n\t"
+                  "      nop                   \n\t"
+                  "      nop                   \n\t"
+                  "      subs  %[n], %[n], #1  \n\t"
+                  "      bne   0b              \n\t"
+                  :
+                  : [n] "r"(n)
+                  : "memory");
+    asm volatile (""::: "memory");
+}
+#    endif
+#endif /* defined(__ARM_ARCH_7M__) || defined(__ARM_ARCH_7EM__) */
