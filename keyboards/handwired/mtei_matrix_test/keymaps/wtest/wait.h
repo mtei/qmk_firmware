202fee8316227b55de996e12295c8ac5f3ca0ef9
mtei <2170248+mtei@users.noreply.github.com>
2020-12-10 21:52:52 +0900

    update local wait.h in handwired/mtei_matrix_test/keymaps/wtest/
    copy `wait_cpuclock` macro from https://github.com/qmk/qmk_firmware/pull/9603

--- keyboards/handwired/mtei_matrix_test/keymaps/wtest/wait.h
+++ keyboards/handwired/mtei_matrix_test/keymaps/wtest/wait.h
@@ -7,6 +7,67 @@
 extern "C" {
 #endif
 
+// copy from #9603
+#if defined(__AVR__)
+#  ifndef wait_cpuclock
+#    define wait_cpuclock(x) __builtin_avr_delay_cycles(x)
+#  endif /* ifndef wait_cpuclock*/
+#endif
+
+#if defined(__ARMEL__) || defined(__ARMEB__)
+#    ifndef __OPTIMIZE__
+#        pragma message "Compiler optimizations disabled; wait_cpuclock() won't work as designed"
+#    endif
+
+#  ifndef wait_cpuclock
+#    define wait_cpuclock(x) wait_cpuclock_allnop(x)
+
+#    define CLOCK_DELAY_NOP8 "nop\n\t nop\n\t nop\n\t nop\n\t   nop\n\t nop\n\t nop\n\t nop\n\t"
+
+__attribute__((always_inline))
+static inline void wait_cpuclock_allnop(unsigned int n) { /* n: 1..135 */
+    /* The argument n must be a constant expression.
+     * That way, compiler optimization will remove unnecessary code. */
+    if (n < 1) { return; }
+    if (n > 8) {
+        unsigned int n8 = n/8;
+        n = n - n8*8;
+        switch (n8) {
+        case 16: asm volatile (CLOCK_DELAY_NOP8::: "memory");
+        case 15: asm volatile (CLOCK_DELAY_NOP8::: "memory");
+        case 14: asm volatile (CLOCK_DELAY_NOP8::: "memory");
+        case 13: asm volatile (CLOCK_DELAY_NOP8::: "memory");
+        case 12: asm volatile (CLOCK_DELAY_NOP8::: "memory");
+        case 11: asm volatile (CLOCK_DELAY_NOP8::: "memory");
+        case 10: asm volatile (CLOCK_DELAY_NOP8::: "memory");
+        case  9: asm volatile (CLOCK_DELAY_NOP8::: "memory");
+        case  8: asm volatile (CLOCK_DELAY_NOP8::: "memory");
+        case  7: asm volatile (CLOCK_DELAY_NOP8::: "memory");
+        case  6: asm volatile (CLOCK_DELAY_NOP8::: "memory");
+        case  5: asm volatile (CLOCK_DELAY_NOP8::: "memory");
+        case  4: asm volatile (CLOCK_DELAY_NOP8::: "memory");
+        case  3: asm volatile (CLOCK_DELAY_NOP8::: "memory");
+        case  2: asm volatile (CLOCK_DELAY_NOP8::: "memory");
+        case  1: asm volatile (CLOCK_DELAY_NOP8::: "memory");
+        case  0: break;
+        }
+    }
+    switch (n) {
+    case 8: asm volatile ("nop"::: "memory");
+    case 7: asm volatile ("nop"::: "memory");
+    case 6: asm volatile ("nop"::: "memory");
+    case 5: asm volatile ("nop"::: "memory");
+    case 4: asm volatile ("nop"::: "memory");
+    case 3: asm volatile ("nop"::: "memory");
+    case 2: asm volatile ("nop"::: "memory");
+    case 1: asm volatile ("nop"::: "memory");
+    case 0: break;
+    }
+}
+#  endif /* ifndef wait_cpuclock*/
+#endif
+// end of copy from #9603
+
 #define WAIT_EXPANDING_NOP_24(n) do { switch (n) { \
     case 24: asm volatile ("nop"::: "memory"); \
     case 23: asm volatile ("nop"::: "memory"); \
